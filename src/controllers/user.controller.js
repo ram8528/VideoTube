b29044d75d86485a56832c9bad98caa3d7699524
ram8528 <106538883+ram8528@users.noreply.github.com>
2025-01-12 19:00:55 +0530

    Implement user login and logout functionality with JWT authentication

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,24 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { apiResponse } from "../utils/apiResponse.js";
 
+const generateAccessAndRefreshTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new apiError(
+      500,
+      "Something went wrong while generating access and refresh tokens"
+    );
+  }
+};
+
 const registerUser = asyncHandler(async (req, res) => {
   // getting user details from frontend
   // validating user details - - not empty
@@ -21,7 +39,7 @@ const registerUser = asyncHandler(async (req, res) => {
     [fullName, email, username, password].some((field) => field?.trim() === "")
   ) {
     throw new apiError(400, "All fields are required");
-  }  // check for empty fields
+  } // check for empty fields
 
   const existedUser = await User.findOne({
     $or: [{ username }, { email }],
@@ -77,4 +95,84 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new apiResponse(200, createdUser, "User Registered Successfully"));
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+  // req body -> database
+  // username or email
+  // find the user
+  // if user exist then check the password
+  // access and refresh token
+  // send cookie
+
+  const { username, email, password } = req.body;
+
+  if (!(username || email)) {  // logically mistake if(!u || !e)
+    throw new apiError(400, "Username or email is required");
+  }
+
+  const user = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (!user) {
+    throw new apiError(404, "User does not exists");
+  }
+
+  const isPasswordValid = await user.isPasswordCorrect(password);
+
+  if (!isPasswordValid) {
+    throw new apiError(401, "Invalid user credentials");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+    user._id
+  );
+
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+  .status(200)
+  .cookie("accessToken", accessToken, options)
+  .cookie("refreshToken", refreshToken,options)
+  .json(
+    new apiResponse(
+      200,
+      {
+        user: loggedInUser,accessToken,refreshToken
+      },
+      "User logged in Successfully"
+    )
+  );
+});
+
+const logoutUser = asyncHandler(async(req,res) => {
+  await User.findByIdAndUpdate(
+    req.user._id,{
+      $set: {
+        refreshToken: undefined
+      }
+    },
+    {
+      new : true
+    }
+  )
+
+  const options = {
+    httpOnly : true,
+    secure : true
+  }
+
+  return res
+  .status(200)
+  .clearCookie("accessToken", options)
+  .clearCookie("refreshToken",options)
+  .json(new apiResponse(200, {}, "User logged out successfully"))
+});
+
+export { registerUser, loginUser ,logoutUser};
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -88,3 +88,5 @@ userSchema.methods.generateRefreshToken = function() {
 }
 
 export const User = mongoose.model('User',userSchema);
+
+// accesstoken short lived refreshtoken long lived
