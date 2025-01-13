01fe3dd245737d839c045ca1f15a3ff3bc680120
ram8528 <106538883+ram8528@users.noreply.github.com>
2025-01-13 12:05:19 +0530

    Add getWatchHistory function to retrieve user's watch history with video owner details

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import {
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  registerUser,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { VerifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -22,8 +34,25 @@ router.route("/register").post(
 router.route("/login").post(loginUser);
 
 //secured routes
-router.route('/logout').post(VerifyJWT, logoutUser);
+router.route("/logout").post(VerifyJWT, logoutUser);
 
-router.route('/refresh-token').post(refreshAccessToken);
+router.route("/refresh-token").post(refreshAccessToken);
 
+router.route("/change-password").post(VerifyJWT, changeCurrentPassword);
+
+router.route("/current-user").get(VerifyJWT, getCurrentUser);
+
+router.route("/update-account").patch(VerifyJWT, updateAccountDetails);
+
+router
+  .route("/avatar")
+  .patch(VerifyJWT, upload.single("avatar"), updateUserAvatar);
+
+router
+  .route("/cover-Image")
+  .patch(VerifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+
+router.route("/c/:username").get(VerifyJWT, getUserChannelProfile);
+
+router.route("/history").get(VerifyJWT, getWatchHistory);
 export default router;
