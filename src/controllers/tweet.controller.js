6bf771c34d95ba2ad1f8289c6d6d3a997a405b75
ram8528 <106538883+ram8528@users.noreply.github.com>
2025-01-15 19:34:58 +0530

    Implement tweet creation, retrieval, update, and deletion with validation checks

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -7,19 +7,125 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
     //TODO: create tweet
+    if(!req.user || !req.user._id) {
+        throw new apiError(401, "User not authenticated");
+    }
+
+    const {content} = req.body;
+
+    if(!content){
+        throw new apiError(400, "Content is required");
+    }
+    // creating new tweet document
+    const tweet = new Tweet({
+        owner : req.user._id, // user id from the authenticated user
+        content
+    })
+
+    await tweet.save();
+
+    const tweetCreated = await Tweet.findById(tweet._id).populate("owner", "username");
+
+    if(!tweetCreated){
+        throw new apiError(500, "Tweet not created");
+    }
+
+    return res
+    .status(200)
+    .json(
+        new apiResponse(
+            200,
+            tweetCreated,
+            "Tweet created successfully"
+        )
+    )
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
     // TODO: get user tweets
-})
+    const userId = req.params.userId; 
+    // const {userId} = req.params;
+
+    if(!isValidObjectId(userId)){
+        throw new apiError(400, "User not authenticated");
+    }
+
+    const tweet = await Tweet.find({ owner : userId}).populate("owner","username");
+
+    if(!tweet || tweet.length === 0) {
+        throw new apiError(404, "No tweets found");
+    }
+
+    return res
+    .status(200)
+    .json(
+        new apiResponse(
+            200,
+            tweet,
+            "User Tweets fetched successfully"
+        )
+    );
+});
 
 const updateTweet = asyncHandler(async (req, res) => {
     //TODO: update tweet
-})
+    const {content} = req.body;
+
+    const {tweetId} = req.params;
+
+    if(!content){
+        throw new apiError(400, "No content found");
+    }
+    
+    const tweet = await Tweet.findById(tweetId);
+
+    if(!tweet) {
+        throw new apiError(404,"Tweet not found");
+    }
+
+    if(tweet.owner.toString() !== req.user._id) {
+        throw new apiError(404, "You are not authorized to update the tweet")
+    }
+
+    tweet.content = content;
+    await tweet.save();
+
+    return res
+    .status(200)
+    .json(
+        new apiResponse(
+            200,
+            tweet,
+            "Tweet updated successfully"
+        )
+    );
+});
 
 const deleteTweet = asyncHandler(async (req, res) => {
     //TODO: delete tweet
-})
+    const {tweetId} = req.params;
+    const tweet = await Tweet.findById(tweetId);
+
+    if(!tweet) {
+        throw new apiError(400, "No tweet found");
+    }
+
+    if(tweet.owner.toString() !== req.user._id) {
+        throw new apiError(400, 'You are not authorized to delete this tweet');
+    }
+
+    await tweet.remove();
+
+    return res
+    .status(200)
+    .json(
+        new apiResponse(
+            200,
+            {},
+            "Tweet deleted successfully"
+        )
+    );
+});
 
 export {
     createTweet,
